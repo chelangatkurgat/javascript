4e02a223832cc6ff65dbf2ccdc5d8288f4c8d413
chelangatkurgat <emkuchela@gmail.com>
2020-04-27 18:16:42 +0300

    few lines of javascript codes

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 //This is my first javascript code//
 console.log('Hello World');
+let name = 'Theodore';
+console.log(name);
+
+//cannot be reserved keyword//
+//should be meaningful and descriptive names
+//cannot start with a number (1name)
+//cannot contain space or hyphen (-)
+//are case sensitive
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//primitive value types
+//let name = 'Theodore'; //string lateral
+//let age = 30; //number lateral
+//let isApproved = true; //Boolean Literal
+//let firstName = undefined;
+//let selectedColor = null;
+
+let person = {
+    name: 'Theodore',
+    Age: 5,
+    weight: 40
+        
+};
+person.name = 'emmy'
+
+console.log(person.name);
+
+
+
+
+
+
