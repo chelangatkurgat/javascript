4af63e06ce535e4f1b36b21c3de2ce8f5649209d
chelangatkurgat <emkuchela@gmail.com>
2020-04-29 14:32:36 +0300

    added for loop function

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+for (let q = 0; q < 5; q++) {
+console.log('basic javascript');
+}
 
- 
+for(let x = 0; x < 5; x++) {
+    console.log('htms and css', x);
+}
+
+for(let y = 0; y <= 5; y++) {
+    if (y % 2 !== 0) console.log(y)
+}
+
+for (let r = 5; r >= 1; r--){
+    if (r % 2 !== 0) console.log(r);
+}
