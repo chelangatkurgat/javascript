f0ab145e69ef8ded53649febe2d90c674d476e8a
chelangatkurgat <emkuchela@gmail.com>
2020-04-29 14:45:19 +0300

    added while loop

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-for (let q = 0; q < 5; q++) {
-console.log('basic javascript');
-}
-
-for(let x = 0; x < 5; x++) {
-    console.log('htms and css', x);
-}
-
-for(let y = 0; y <= 5; y++) {
-    if (y % 2 !== 0) console.log(y)
-}
-
-for (let r = 5; r >= 1; r--){
-    if (r % 2 !== 0) console.log(r);
+let q = 0;
+while (q <= 5) {
+    if (q % 2 !== 0) console.log(q);
+    q++;
 }
