5b4598be583e454aa29b9f22732b030d1af0f77e
chelangatkurgat <emkuchela@gmail.com>
2020-04-29 15:10:51 +0300

    added for-in loop codes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let q = 0;
-while (q <= 5) {
-    if (q % 2 !== 0) console.log(q);
-    q++;
-}
+const person = {
+    name: 'Emmy',
+    age: 30
+};
+
+for (let key in person)
+    // console.log(key);
+
+    console.log(key, person[key]);
+
+const colors = ['red', 'blue', 'green']
+
+for (let index in colors)
+// console.log(index);
+
+console.log(index, colors[index]);
+
