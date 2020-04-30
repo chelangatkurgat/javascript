30de4eb5212111349f193ba770361f194fbf7a7b
chelangatkurgat <emkuchela@gmail.com>
2020-04-30 16:01:41 +0300

    added sum of multiples

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const movie = {
-    title: 'a',
-    releaseYear: 2018,
-    rating: 4.5,
-    director: 'b',
-};
+console.log(sum(10));
 
-showProperties(movie);
+//multiples of 3: 3, 6, 9
+//multiples of 5: 5, 10
 
-function showProperties(obj){
-    for (let key in obj)
-        if (typeof obj [key] === 'string')
-    console.log(key, obj[key]);
+function sum(limit) {
+    let sum = 0;
+    
+    for (let i = 0; i <= limit; i++)
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i;
+
+    return sum;
+
 }
