a0676c725cbad0d6d18d7caa1f0dab02be9aa850
chelangatkurgat <emkuchela@gmail.com>
2020-05-03 12:41:04 +0300

    added factorial function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-let total = 0, count = 1;
-while ( count <= 10){
-    total += count;
-    count += 1;
+function factorial(n) {
+    if (n == 0){
+        return 1;
+    }
+    else {
+        return factorial(n - 1) * n;
+    }
 }
-console.log(total);
+console.log(factorial(8));
 
