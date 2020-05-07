526303fe9d97d5824275fdb6e42bc1b207eb0751
chelangatkurgat <emkuchela@gmail.com>
2020-05-07 22:39:41 +0300

    small codes

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ var name = "Theodore"
 const greetings = "Hello "
 console.log(greetings + name);
 
+let math = "2, 4";
 
+console.log(math);
+
+let theNumber = Number (prompt("pick a number"))
+if (!Number.isNaN(theNumber)) {
+    console.log("Your number is the square root of " + theNumber * theNumber);
+
+} else {
+    console.log("Hey. Why did't you give me a number")
+}
