0b8ce33d2fbb1fc2aedf63ee85f03b38705bcaed
chelangatkurgat <emkuchela@gmail.com>
2020-05-11 18:30:23 +0300

    small code

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ if (1 < 2) {
 for (let number = 0; number <= 12; number = number + 2){
     console.log(number);
 }
+
+for (let current = 20; ; current = current + 1) {
+    if (current % 7 == 0){
+        console.log(current);
+        break;
+    }
+}
+
