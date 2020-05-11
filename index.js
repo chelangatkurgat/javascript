a1fd4d28da82c1ac2366898c9ada92f4dc7f7962
chelangatkurgat <emkuchela@gmail.com>
2020-05-11 18:05:20 +0300

    for loop code

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ while (number <= 12) {
     console.log(number);
     number = number + 2;
 }
+
+if (false != true) {
+    console.log("That make sense");
+
+if (1 < 2) {
+    console.log("No surprise there")
+}
+}
+for (let number = 0; number <= 12; number = number + 2){
+    console.log(number);
+}
